95a3b75aa742339de5172f6aed983b6599beec75
Sota-Kitano <sota.kitano@force-corp.co.jp>
2021-07-27 18:30:06 +0900

    contact alart correct

--- js.js
+++ js.js
@@ -1,18 +1,40 @@
+'use strict'
 
-// $(function(){
-//     $('a[href^="#"]').click(function() {
-//     var speed = 800;
-//     var href= $(this).attr("href");
-//     var target = $(href == "#" || href == "" ? 'html' : href);
-//     var position = target.offset().top;
-//     $('body,html').animate({scrollTop:position}, speed, 'swing');
-//     return false;
-//     });
-//     });
-
-    $('.inner nav ul li #career').click(function () {
-        var elmHash = $(this).attr('href'); //hrefの内容を取得
-        var pos = Math.round($(elmHash).offset().top-120);  //headerの高さを引く
-        $('career').animate({scrollTop: pos}, 500);//取得した位置にスクロール※数値が大きいほどゆっくりスクロール
-        return false;//リンクの無効化
-      });
+function check(){
+  if (mail_form.contact_name.value == ""){
+      //条件に一致する場合(メールアドレスが空の場合)
+      alert ("お名前を入力してください");    //エラーメッセージを出力
+      return false;    //送信ボタン本来の動作をキャンセルします
+  }else{
+      //条件に一致しない場合(メールアドレスが入力されている場合)
+      return true;    //送信ボタン本来の動作を実行します
+  }
+}
+function check(){
+  if (contactTelNum.value == ""){
+      //条件に一致する場合(メールアドレスが空の場合)
+      alert ("電話番号を入力してください");    //エラーメッセージを出力
+      return false;    //送信ボタン本来の動作をキャンセルします
+  }else{
+      //条件に一致しない場合(メールアドレスが入力されている場合)
+      return true;    //送信ボタン本来の動作を実行します
+  }
+}function check(){
+  if (contactMail.value == ""){
+      //条件に一致する場合(メールアドレスが空の場合)
+      alert ("メールアドレスを入力してください");    //エラーメッセージを出力
+      return false;    //送信ボタン本来の動作をキャンセルします
+  }else{
+      //条件に一致しない場合(メールアドレスが入力されている場合)
+      return true;    //送信ボタン本来の動作を実行します
+  }
+}function check(){
+  if (contactContent.value == ""){
+      //条件に一致する場合(メールアドレスが空の場合)
+      alert ("お問い合わせ内容を入力してください");    //エラーメッセージを出力
+      return false;    //送信ボタン本来の動作をキャンセルします
+  }else{
+      //条件に一致しない場合(メールアドレスが入力されている場合)
+      return true;    //送信ボタン本来の動作を実行します
+  }
+}
