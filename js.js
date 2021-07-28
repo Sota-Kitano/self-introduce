d25f09f61cc587583991e0ea6d72dbc0b098dd74
Sota-Kitano <sota.kitano@force-corp.co.jp>
2021-07-28 18:03:55 +0900

    only sendwindow, others correct

--- js.js
+++ js.js
@@ -1,40 +1,52 @@
 'use strict'
 
-function check(){
-  if (mail_form.contact_name.value == ""){
-      //条件に一致する場合(メールアドレスが空の場合)
-      alert ("お名前を入力してください");    //エラーメッセージを出力
-      return false;    //送信ボタン本来の動作をキャンセルします
-  }else{
-      //条件に一致しない場合(メールアドレスが入力されている場合)
-      return true;    //送信ボタン本来の動作を実行します
+
+
+
+// 送信チェック
+
+function ButtonClick() {
+  var Name;
+  var TelNum;
+  var Mail;
+  var Contents;
+  Name=document.mail_form.yourname;
+  TelNum=document.mail_form.telnum;
+  Mail=document.mail_form.mailaddress;
+  Contents=document.mail_form.contents;
+  
+  if(Name.value==""){
+  alert("名前を入力してください");
+  Name.focus();
+  return false;
   }
-}
-function check(){
-  if (contactTelNum.value == ""){
-      //条件に一致する場合(メールアドレスが空の場合)
-      alert ("電話番号を入力してください");    //エラーメッセージを出力
-      return false;    //送信ボタン本来の動作をキャンセルします
-  }else{
-      //条件に一致しない場合(メールアドレスが入力されている場合)
-      return true;    //送信ボタン本来の動作を実行します
+  if(TelNum.value=="") {
+  alert("電話番号を入力してください");
+  TelNum.focus();
+  return false;
   }
-}function check(){
-  if (contactMail.value == ""){
-      //条件に一致する場合(メールアドレスが空の場合)
-      alert ("メールアドレスを入力してください");    //エラーメッセージを出力
-      return false;    //送信ボタン本来の動作をキャンセルします
-  }else{
-      //条件に一致しない場合(メールアドレスが入力されている場合)
-      return true;    //送信ボタン本来の動作を実行します
+  if(Mail.value=="") {
+  alert("メールアドレスを入力してください");
+  Mail.focus();
+  return false;
   }
-}function check(){
-  if (contactContent.value == ""){
-      //条件に一致する場合(メールアドレスが空の場合)
-      alert ("お問い合わせ内容を入力してください");    //エラーメッセージを出力
-      return false;    //送信ボタン本来の動作をキャンセルします
-  }else{
-      //条件に一致しない場合(メールアドレスが入力されている場合)
-      return true;    //送信ボタン本来の動作を実行します
+  if(Contents.value=="") {
+  alert("電話番号を入力してください");
+  Contents.focus();
+  return false;
   }
-}
+  document.Name.value = navigator.userAgent;
+  alert ('送信しますか?');
+  return true;
+  }
+
+// }function checkcont(){
+//   if (){
+//       //条件に一致する場合(メールアドレスが空の場合)
+//       alert ("お問い合わせ内容を入力してください");    //エラーメッセージを出力
+//       return false;    //送信ボタン本来の動作をキャンセルします
+//   }else{
+//       //条件に一致しない場合(メールアドレスが入力されている場合)
+//       return true;    //送信ボタン本来の動作を実行します
+//   }
+// }
